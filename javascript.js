4b0d269c0732a84af95fee2307bbd0ac1482b4e9
AntonyMbuvi <mwendwambuvi254@gmail.com>
2023-08-17 23:38:49 +0300

    Added a way to count the results for each match

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 function getComputerChoice(min , max){
     let number = Math.floor(Math.random() * (max - min) + min);
-    console.log(number)
     let choice;
     switch(number) {
         case(1):
@@ -15,31 +14,57 @@ function getComputerChoice(min , max){
          choice = 'scissors';
         break;
     }
-    console.log(choice);
     return choice;
 }
-let max = 4;
-let min = 1;
 
-computerSelection = getComputerChoice(max, min);
 
 function referee(computerSelection, playerSelection){
     let verdict;
     if ((computerSelection == 'rock' && playerSelection == 'scissors') || (computerSelection == 'paper' && playerSelection == 'rock') || (computerSelection == 'scissors' && playerSelection == 'paper'))
     {
-        verdict = `You Lose! ${computerSelection} beats ${playerSelection}`
+        verdict = `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
     else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper'))
     {
-        verdict = `You Win! ${playerSelection} beats ${computerSelection}`
+        verdict = `You Win! ${playerSelection} beats ${computerSelection}`;
     }
     else{
-        verdict = `Its a draw, please try again`
+        verdict = `Its a draw, please try again`;
     }
-    console.log(verdict)
     return verdict;
 }
-playerSelection = 'rock';
-playerSelection = playerSelection.toLowerCase();
 
-verdict = referee(computerSelection, playerSelection)
+
+
+function game () {
+    let wins = 0;
+    let losses = 0;
+    let draws = 0;
+
+    playerSelection = 'rock';
+    playerSelection = playerSelection.toLowerCase();
+
+    let max = 4;
+    let min = 1;
+    for (let i = 0; i < 5; i++) {
+        
+        computerSelection = getComputerChoice(max, min);
+        verdict = referee(computerSelection, playerSelection);
+        console.log(verdict);
+
+        if (verdict == 'Its a draw, please try again'){
+            draws++;
+        }
+        else if (verdict == 'You Lose! paper beats rock'){
+            losses++;
+        }
+        else{
+            wins++;
+        }
+    }
+
+    console.log(`You have ${wins} wins, ${losses} losses ${draws} draws`)
+
+}
+
+game()
